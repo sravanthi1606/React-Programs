c9214404bd22af23ee8351a2d9ac928f01d434bc
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2024-02-08 09:31:52 +0530

    contactFun.js

---
+++ FunctionComponent/contactFun.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contactfun(props){
+    return (
+    <div className="container">
+        <h1>Contact Function Component</h1>
+        <h2>{props.name}</h2>
+    </div>
+    )
+}
+
+export default Contactfun
