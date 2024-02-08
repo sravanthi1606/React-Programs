8eac481b9ca45fd5091fed5c6332593d80e90aab
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2024-02-08 09:33:12 +0530

    serviceFun.js

---
+++ FunctionComponent/serviceFun.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Servicefun(props){
+    return(
+    <div className="container-fluid">
+        <h1>Service Function Component</h1>
+        <h2>{props.name}</h2>
+    </div>
+    )
+}
+export default Servicefun
