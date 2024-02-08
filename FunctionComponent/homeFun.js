b3432470374b720d48b6cdefc73c54dd7accddf3
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2024-02-08 09:32:28 +0530

    homeFun.js

---
+++ FunctionComponent/homeFun.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homefun(props){
+    return(
+    <div className="container">
+        <h1>Home Function Component</h1>
+        <h2>{props.name}</h2>
+    </div>
+    )
+}
+export default Homefun
