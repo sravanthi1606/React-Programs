55cf8f4be746b2cddd88f6f62a90897e2acef95b
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2024-02-22 16:31:07 +0530

    Updated Home.js

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-// import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Home=()=>{
-    // const location = useLocation();
-    // console.log(location);
+    const location = useLocation();
     return(
         <div>
             <h1>This is home Page </h1>
-            {/* <h3>{location.state.email}</h3> */}
+            <h3>{location.state.email}</h3> 
         </div>
     )
 }
